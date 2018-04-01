0ab4ec8de9c0f072c2fba0fbdd5b5ba3e5fecda6
garycheung13 <garyc102@gmail.com>
2018-04-01 01:45:43 -0400

    remove unused playlist routes

--- routes/api.js
+++ routes/api.js
@@ -17,8 +17,10 @@ const xmlparser = new xml2js.Parser({
 });
 
 const sanitizeHtmlConfig = {
-  allowedTags: [],
-  allowedAttributes: []
+  allowedTags: ["br", "a"],
+  allowedAttributes: {
+    'a': ['href']
+  }
 };
 
 function handleValues(value) {
@@ -28,26 +30,6 @@ function handleValues(value) {
 function stripNewLine(name) {
   return name.replace(/\n/g, '');
 }
-
-const mockPlaylist = [{
-  podcastEpisodeTitle: "#626: White Haze",
-  podcastFrom: "This American Life",
-  link: "https://www.podtrac.com/pts/redirect.mp3/podcast.thisamericanlife.org/podcast/626.mp3",
-  summary: "Right-wing groups like the Proud Boys say they have no tolerance for racism or white supremacist groups. Their leader Gavin McInnes disavowed the white nationalist rally in Charlottesville. But the Proud Boys believe “the West is the best,” which, one of them points out, is not such a big jump from “whites are best.” And one of the Proud Boys organized the Charlottesville rally. (The group now claims he was a spy.) What should we make of groups like this?",
-  lastPlayPos: 0
-}, {
-  podcastEpisodeTitle: "Brain Chemistry",
-  podcastFrom: "The Truth",
-  link: "https://www.podtrac.com/pts/redirect.mp3/traffic.libsyn.com/secure/thetruthapm/Brain_Chemistry.mp3?dest-id=90480",
-  summary: "A cryogenically-frozen man is revived in the future, to a world that is not quite what he expected.</p> <p>Written by Eric Molinsky, and produced by Jonathan Mitchell. Performed by Scott Adsit, Amy Warren, and Ed Herbstman, with Anna Neu, Billy Griffin, Alexis Lambright, Louis Kornfeld, Elana Fishbein, Joanna Hausmann, and Kerry Kastin.",
-  lastPlayPos: 0
-}, {
-  podcastEpisodeTitle: "#24 | Watergate",
-  podcastFrom: "Twenty Thousand Hertz",
-  link: "https://traffic.libsyn.com/secure/20khz/Watergate_v4.mp3?dest-id=440475",
-  summary: "How did an open reel tape recorder secretly planted in the White House basement lead to the demise of the 37th President of the United States?",
-  lastPlayPos: 0
-}]
 
 // implamentation based on this stackoverflow question
 // https://stackoverflow.com/questions/34327599/using-express-js-to-consume-an-api
@@ -128,34 +110,10 @@ router.get('/parser/:rssFeed', cache("1 hour"), function (req, res, next) {
   });
 });
 
-router.get('/api/*', function (req, res, next) {
+router.get('*', function (req, res, next) {
   return res.json({
-    error: "404: no route found"
+    message: "No endpoint found"
   })
-});
-
-//load a demo playlist
-//temp endpoint
-router.get('/demo/playlist', function (req, res, next) {
-  return res.json(mockPlaylist);
-});
-
-
-router.post('/demo/playlist', function (req, res) {
-  mockPlaylist.push(req.body.episode);
-  return res.json(mockPlaylist);
-});
-
-
-router.get('/', function (req, res, next) {
-  // And insert something like this instead:
-  return res.json([{
-    id: 1,
-    username: "samsepi0l"
-  }, {
-    id: 2,
-    username: "D0loresH4ze"
-  }]);
 });
 
 
